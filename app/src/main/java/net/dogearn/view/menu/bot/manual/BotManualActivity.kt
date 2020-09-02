bd4d185f4033d35c6b0139b073b4795b8b7e1be8
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-09-02 16:30:04 +0700

    Update BotManualActivity.kt
    add mide in manual bot

--- app/src/main/java/net/dogearn/view/menu/bot/manual/BotManualActivity.kt
+++ app/src/main/java/net/dogearn/view/menu/bot/manual/BotManualActivity.kt
@@ -1,28 +1,38 @@
 package net.dogearn.view.menu.bot.manual
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.*
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import net.dogearn.MainActivity
 import net.dogearn.R
+import net.dogearn.config.BackgroundGetDataUser
+import net.dogearn.config.BackgroundServiceBalance
 import net.dogearn.config.BitCoinFormat
 import net.dogearn.config.Loading
 import net.dogearn.controller.DogeController
 import net.dogearn.controller.WebController
+import net.dogearn.model.Setting
 import net.dogearn.model.User
 import org.json.JSONObject
 import java.math.BigDecimal
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.concurrent.schedule
 
 class BotManualActivity : AppCompatActivity() {
   private lateinit var user: User
+  private lateinit var config: Setting
   private lateinit var bitCoinFormat: BitCoinFormat
   private lateinit var loading: Loading
   private lateinit var response: JSONObject
+  private lateinit var fundText: TextView
   private lateinit var balanceText: TextView
   private lateinit var gradeText: TextView
   private lateinit var highText: TextView
@@ -38,18 +48,28 @@ class BotManualActivity : AppCompatActivity() {
   private lateinit var payIn: BigDecimal
   private lateinit var profit: BigDecimal
   private lateinit var startBalance: BigDecimal
+  private lateinit var intentServiceGetDataUser: Intent
+  private lateinit var intentServiceBalance: Intent
+  private lateinit var goTo: Intent
   private var high = BigDecimal(5)
   private var maxRow = 10
   private var seed = (0..99999).random().toString()
+  private lateinit var percentTable: ArrayList<Double>
+  private var defaultMaximum = 0.01
+  private var percent = 0.01
+  private var maxBalance = BigDecimal(0)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_bot_manual)
 
     user = User(this)
+    config = Setting(this)
     bitCoinFormat = BitCoinFormat()
     loading = Loading(this)
+    percentTable = ArrayList()
 
+    fundText = findViewById(R.id.textViewFund)
     balanceText = findViewById(R.id.textViewBalance)
     gradeText = findViewById(R.id.textViewGrade)
     highText = findViewById(R.id.textViewHigh)
@@ -62,18 +82,26 @@ class BotManualActivity : AppCompatActivity() {
     resultLinearLayout = findViewById(R.id.linearLayoutResult)
     statusLinearLayout = findViewById(R.id.linearLayoutStatus)
 
+    setListTargetMaximum()
+
     highText.text = "Possibility: ${high * BigDecimal(10)}%"
     balanceText.text = intent.getStringExtra("balanceView")
     balance = intent.getStringExtra("balance")!!.toBigDecimal()
     gradeText.text = intent.getStringExtra("grade")
     startBalance = balance
+    maxBalance = bitCoinFormat.dogeToDecimal(bitCoinFormat.decimalToDoge(balance).multiply(percent.toBigDecimal()))
+    fundText.text = "Maximum : ${bitCoinFormat.decimalToDoge(balance).multiply(percent.toBigDecimal()).toPlainString()}"
 
     stakeButton.setOnClickListener {
       if (inputBalance.text.isEmpty()) {
         Toast.makeText(this, "Amount cant not be empty", Toast.LENGTH_SHORT).show()
       } else {
         payIn = bitCoinFormat.dogeToDecimal(inputBalance.text.toString().toBigDecimal())
-        onBetting()
+        if (payIn > maxBalance) {
+          Toast.makeText(this, "Doge you can input should not be more than ${bitCoinFormat.decimalToDoge(balance).multiply(percent.toBigDecimal()).toPlainString()}", Toast.LENGTH_LONG).show()
+        } else {
+          onBetting()
+        }
       }
     }
 
@@ -88,6 +116,11 @@ class BotManualActivity : AppCompatActivity() {
           highSeekBar.progress = 9
           getProgress = 9
         }
+        if (progress >= 0 || progress <= 10) {
+          percent = defaultMaximum * percentTable[getProgress - 1]
+        }
+        maxBalance = bitCoinFormat.dogeToDecimal(bitCoinFormat.decimalToDoge(balance).multiply(percent.toBigDecimal()))
+        fundText.text = "Maximum : ${bitCoinFormat.decimalToDoge(balance).multiply(percent.toBigDecimal()).toPlainString()}"
         high = getProgress.toBigDecimal()
         highText.text = "Possibility: ${getProgress * 10}%"
       }
@@ -100,9 +133,53 @@ class BotManualActivity : AppCompatActivity() {
   }
 
   override fun onBackPressed() {
+    stopService(intentServiceBalance)
+    stopService(intentServiceGetDataUser)
     finishAffinity()
-    val goTo = Intent(this, MainActivity::class.java)
+    goTo = Intent(this, MainActivity::class.java)
     startActivity(goTo)
+  }
+
+  override fun onStop() {
+    LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiverWebLogout)
+    LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiverDoge)
+    stopService(intentServiceBalance)
+    stopService(intentServiceGetDataUser)
+    super.onStop()
+  }
+
+  override fun onStart() {
+    super.onStart()
+    Timer().schedule(1000) {
+      intentServiceBalance = Intent(applicationContext, BackgroundServiceBalance::class.java)
+      startService(intentServiceBalance)
+
+      LocalBroadcastManager.getInstance(applicationContext).registerReceiver(broadcastReceiverDoge, IntentFilter("net.dogearn.doge"))
+
+      intentServiceGetDataUser = Intent(applicationContext, BackgroundGetDataUser::class.java)
+      startService(intentServiceGetDataUser)
+
+      LocalBroadcastManager.getInstance(applicationContext).registerReceiver(broadcastReceiverWebLogout, IntentFilter("net.dogearn.web.logout"))
+    }
+  }
+
+  private var broadcastReceiverWebLogout: BroadcastReceiver = object : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+      if (intent.getBooleanExtra("isLogout", false)) {
+        onLogout()
+      }
+    }
+  }
+  private var broadcastReceiverDoge: BroadcastReceiver = object : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+      balance = intent.getSerializableExtra("balanceValue") as BigDecimal
+      user.setString("balanceValue", balance.toPlainString())
+      user.setString("balanceText", "${BitCoinFormat().decimalToDoge(balance).toPlainString()} DOGE")
+      balanceText.text = user.getString("balanceText")
+      maxBalance = bitCoinFormat.dogeToDecimal(bitCoinFormat.decimalToDoge(balance).multiply(percent.toBigDecimal()))
+      val textFund = "Maximum : ${bitCoinFormat.decimalToDoge(balance).multiply(percent.toBigDecimal()).toPlainString()}"
+      fundText.text = textFund
+    }
   }
 
   private fun onBetting() {
@@ -136,7 +213,7 @@ class BotManualActivity : AppCompatActivity() {
 
           setView(bitCoinFormat.decimalToDoge(payIn).toPlainString(), fundLinearLayout, false, winBot)
           setView("${high.multiply(BigDecimal(10))}%", highLinearLayout, false, winBot)
-          setView(BitCoinFormat().decimalToDoge(balanceRemaining).toPlainString(), resultLinearLayout, false, winBot)
+          setView(BitCoinFormat().decimalToDoge(puyOut).toPlainString(), resultLinearLayout, false, winBot)
           if (winBot) {
             setView("WIN", statusLinearLayout, false, winBot)
             stakeButton.visibility = Button.GONE
@@ -204,6 +281,41 @@ class BotManualActivity : AppCompatActivity() {
       linearLayout.addView(valueView, 1)
     } else {
       linearLayout.addView(valueView)
+    }
+  }
+
+  private fun setListTargetMaximum() {
+    percentTable.add(0.2)
+    percentTable.add(0.3)
+    percentTable.add(0.4)
+    percentTable.add(0.5)
+    percentTable.add(1.0)
+    percentTable.add(2.0)
+    percentTable.add(3.0)
+    percentTable.add(4.0)
+    percentTable.add(5.0)
+  }
+
+  private fun onLogout() {
+    Timer().schedule(100) {
+      response = WebController.Get("user.logout", user.getString("token")).execute().get()
+      if (response.getInt("code") == 200) {
+        user.clear()
+        config.clear()
+        goTo = Intent(applicationContext, MainActivity::class.java)
+        loading.closeDialog()
+        startActivity(goTo)
+        finishAffinity()
+      } else {
+        if (response.getString("data").contains("Unauthenticated.")) {
+          user.clear()
+          config.clear()
+          goTo = Intent(applicationContext, MainActivity::class.java)
+          loading.closeDialog()
+          startActivity(goTo)
+          finishAffinity()
+        }
+      }
     }
   }
 }
